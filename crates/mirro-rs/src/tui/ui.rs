e6a0b6f403bdbe3651787a9d1f08a168333800e5
kawaki-san <dev@kanjala.com>
2022-12-21 00:29:17 +0200

    ui: change log colours

--- crates/mirro-rs/src/tui/ui.rs
+++ crates/mirro-rs/src/tui/ui.rs
@@ -194,10 +194,12 @@ fn centered_rect(percent_x: u16, percent_y: u16, area: Rect) -> Rect {
 fn draw_logs<'a>() -> TuiLoggerWidget<'a> {
     TuiLoggerWidget::default()
         .style_error(Style::default().fg(Color::Red))
-        .style_debug(Style::default().fg(Color::Green))
+        .style_debug(Style::default().fg(Color::Blue))
         .style_warn(Style::default().fg(Color::Yellow))
-        .style_trace(Style::default().fg(Color::Gray))
-        .style_info(Style::default().fg(Color::Blue))
+        .style_trace(Style::default().fg(Color::Magenta))
+        .style_info(Style::default().fg(Color::Green))
+        .output_file(false)
+        .output_target(false)
         .block(
             Block::default()
                 .title(title("Logs"))
