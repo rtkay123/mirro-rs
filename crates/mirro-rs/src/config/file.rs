d9858641d481fe478d813d8911859a55b19a2127
kawaki-san <dev@kanjala.com>
2022-12-23 15:45:46 +0200

    feat: add multiple config file backends

--- crates/mirro-rs/src/config/file.rs
+++ crates/mirro-rs/src/config/file.rs
@@ -1,25 +1,42 @@
-use std::{io::ErrorKind, path::PathBuf};
+use std::{fmt::Display, io::ErrorKind, path::PathBuf};
 
 use crate::cli::Args;
 
 pub fn read_config_file() -> (Args, Option<PathBuf>) {
-    let config_file = dirs::config_dir().map(|mut dir| {
-        let crate_name = env!("CARGO_PKG_NAME");
-        let mut location = PathBuf::from(crate_name);
-        let mut file = PathBuf::from(crate_name);
-        file.set_extension("toml");
-        location.push(file.clone());
-
-        let mut alt = dir.clone();
-        dir.push(location);
-        alt.push(file);
-        check_file(&dir, Some(&alt))
+    let config_file = dirs::config_dir().map(|dir| {
+        let is_yaml = extension() == Config::Yaml;
+        match get_config(dir.clone(), &extension().to_string()) {
+            Some(res) => Some(res),
+            None => {
+                if is_yaml {
+                    get_config(dir, "yml")
+                } else {
+                    None
+                }
+            }
+        }
     });
     match config_file {
         Some(Some(opts)) => opts,
         _ => {
-            let config = include_str!("../../../../examples/mirro-rs.toml");
-            let config: Args = toml::from_str(config).unwrap();
+            #[cfg(feature = "config-toml")]
+            let config_str = include_str!("../../../../examples/mirro-rs.toml");
+
+            #[cfg(feature = "config-yaml")]
+            let config_str = include_str!("../../../../examples/mirro-rs.yaml");
+
+            #[cfg(feature = "config-json")]
+            let config_str = include_str!("../../../../examples/mirro-rs.json");
+
+            #[cfg(feature = "config-toml")]
+            let config: Args = toml::from_str(config_str).unwrap();
+
+            #[cfg(feature = "config-json")]
+            let config: Args = serde_json::from_str(config_str).unwrap();
+
+            #[cfg(feature = "config-yaml")]
+            let config: Args = serde_yaml::from_str(config_str).unwrap();
+
             (config, None)
         }
     }
@@ -40,13 +57,24 @@ fn check_file(file: &PathBuf, backup: Option<&PathBuf>) -> Option<(Args, Option<
 
     let f = std::fs::read_to_string(file);
     match f {
-        Ok(contents) => match toml::from_str::<Args>(&contents) {
-            Ok(e) => Some((e, Some(file.to_owned()))),
-            Err(e) => {
-                err(format!("config: {} -> {}", file.display(), e));
-                call_backup(backup)
+        Ok(contents) => {
+            #[cfg(feature = "config-toml")]
+            let result: Result<Args, _> = toml::from_str::<Args>(&contents);
+
+            #[cfg(feature = "config-json")]
+            let result: Result<Args, _> = serde_json::from_str::<Args>(&contents);
+
+            #[cfg(feature = "config-yaml")]
+            let result: Result<Args, _> = serde_yaml::from_str::<Args>(&contents);
+
+            match result {
+                Ok(e) => Some((e, Some(file.to_owned()))),
+                Err(e) => {
+                    err(format!("config: {} -> {}", file.display(), e));
+                    call_backup(backup)
+                }
             }
-        },
+        }
         Err(e) => {
             if e.kind() != ErrorKind::NotFound {
                 err(format!("config: {} -> {}", file.display(), e));
@@ -54,4 +82,47 @@ fn check_file(file: &PathBuf, backup: Option<&PathBuf>) -> Option<(Args, Option<
             call_backup(backup)
         }
     }
+}
+
+fn extension() -> Config {
+    if cfg!(feature = "config-json") {
+        Config::Json
+    } else if cfg!(feature = "config-yaml") {
+        Config::Yaml
+    } else {
+        Config::Toml
+    }
+}
+
+#[derive(PartialEq, Eq)]
+enum Config {
+    Json,
+    Toml,
+    Yaml,
+}
+
+impl Display for Config {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Config::Json => "json",
+                Config::Toml => "toml",
+                Config::Yaml => "yaml",
+            }
+        )
+    }
+}
+
+fn get_config(mut dir: PathBuf, extension: &str) -> Option<(Args, Option<PathBuf>)> {
+    let crate_name = env!("CARGO_PKG_NAME");
+    let mut location = PathBuf::from(crate_name);
+    let mut file = PathBuf::from(crate_name);
+    file.set_extension(extension);
+    location.push(file.clone());
+    let mut alt = dir.clone();
+    dir.push(location);
+    alt.push(file);
+    check_file(&dir, Some(&alt))
 }
