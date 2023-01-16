3ba7d2bd875be61452f46f7c686ad27e7dc2ef2e
kawaki-san <dev@kanjala.com>
2023-01-16 18:59:17 +0200

    refactor: use `outdir` for man page and completion artifacts

--- crates/mirro-rs/build.rs
+++ crates/mirro-rs/build.rs
@@ -6,26 +6,29 @@ mod cli;
 
 fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=src/cli/mod.rs");
-    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"));
+    if let Some(outdir) = std::env::var_os("OUT_DIR") {
+        let outdir = std::path::PathBuf::from(outdir);
+        let man_dir = outdir.join("man");
+        std::fs::create_dir_all(&man_dir)?;
 
-    let man_dir = outdir.join("man");
+        let mut command = cli::ArgConfig::command();
 
-    let mut command = cli::ArgConfig::command();
+        let man = clap_mangen::Man::new(command.clone());
+        let mut buffer: Vec<u8> = Default::default();
+        man.render(&mut buffer)?;
 
-    let man = clap_mangen::Man::new(command.clone());
-    let mut buffer: Vec<u8> = Default::default();
-    man.render(&mut buffer)?;
+        std::fs::write(man_dir.join("mirro-rs.1"), buffer)?;
 
-    std::fs::write(man_dir.join("mirro-rs.1"), buffer)?;
+        let completions_dir = outdir.join("completions");
+        std::fs::create_dir_all(&completions_dir)?;
 
-    let completions_dir = outdir.join("completions");
+        let crate_name = env!("CARGO_PKG_NAME");
 
-    let crate_name = env!("CARGO_PKG_NAME");
-
-    generate_to(Shell::Zsh, &mut command, crate_name, &completions_dir)?;
-    generate_to(Shell::Bash, &mut command, crate_name, &completions_dir)?;
-    generate_to(Shell::Fish, &mut command, crate_name, &completions_dir)?;
-    generate_to(Shell::Elvish, &mut command, crate_name, &completions_dir)?;
+        generate_to(Shell::Zsh, &mut command, crate_name, &completions_dir)?;
+        generate_to(Shell::Bash, &mut command, crate_name, &completions_dir)?;
+        generate_to(Shell::Fish, &mut command, crate_name, &completions_dir)?;
+        generate_to(Shell::Elvish, &mut command, crate_name, &completions_dir)?;
+    }
 
     Ok(())
 }
